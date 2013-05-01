a692917b222a75befd8ce313d57d3292d17817b1
Hack Reactor Students <github@hackreactor.com>
2013-05-01 11:26:50 -0700

    Add test for getIndexBelowMaxForKey()

--- spec/hashTableSpec.js
+++ spec/hashTableSpec.js
@@ -10,5 +10,9 @@ describe("hashTable", function() {
     expect(hashTable.retrieve).toEqual(jasmine.any(Function));
   });
 
+  it("getIndexBelowMaxForKey(string, limit) should return number < limit", function() {
+    expect(getIndexBelowMaxForKey('Dog', hashTable._limit)).toBeLessThan(hashTable._limit);
+  });
+
   // add more tests here to test the functionality of hashTable
 });
