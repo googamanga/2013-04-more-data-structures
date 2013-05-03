7bc420aa4da45995205cfd80f6b8dd37e903d393
Hack Reactor Students <github@hackreactor.com>
2013-05-03 15:33:49 -0700

    Get started on Binary Search tree, included underscore.js

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,2 +1,25 @@
-var makeBinarySearchTree = function(){
+////////////////////////////////
+/*
+Implement a binarySearchTree class with the following properties:
+A .left property, a binary search tree (BST) where all values are lower than than it the current value.
+A .right property, a BST where all values are higher than than it the current value.
+A .insert() method, which accepts a value and places in the tree in the correct position.
+A .contains() method, which accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
+A .depthFirstLog() method, which accepts a callback and executes it on every value contained in the tree.
+  */
+var makeBinarySearchTree = function(value){
+  return {
+    value: value,
+    left: null,
+    right: null,
+    insert: function(){
+
+    },
+    contains: function(){
+
+    },
+    depthFirstLog: function(callback){
+
+    }
+  };
 };
