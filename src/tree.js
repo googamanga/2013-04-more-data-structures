50f3d4123a800d61265201b1015dee62fa4e79e6
Hack Reactor Students <github@hackreactor.com>
2013-05-03 12:40:22 -0700

    Add tree.detachFromParent with tests

--- src/tree.js
+++ src/tree.js
@@ -2,6 +2,8 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = [];
+  newTree.parent = null;
+  newTree.detachFromParent = treeMethods.detachFromParent;
   newTree.addChild = treeMethods.addChild;
   newTree.contains = treeMethods.contains;
   return newTree;
@@ -11,6 +13,7 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value){
   var newChild = makeTree(value);
+  newChild.parent = this;
   this.children.push(newChild);
   return newChild;
 };
@@ -23,4 +26,25 @@ treeMethods.contains = function(value){
     if (this.children[i].contains(value)) return true;
   }
   return false;
+};
+
+treeMethods.detachFromParent = function() {
+  var tree = this;
+  // find and remove tree from parent's children array
+  if (tree.parent) {
+    var parent = tree.parent;
+    for (var j = 0; j < parent.children.length; j++) {
+      if (parent.children[j] === tree) {
+        break;
+      }
+    }
+    if (j < parent.children.length) {
+      parent.children.splice(j, 1);
+    } else {
+      throw new Error("Corrupt Tree. Can't remove child from parent");
+    }
+  }
+
+  tree.parent = null;
+  return tree;
 };
